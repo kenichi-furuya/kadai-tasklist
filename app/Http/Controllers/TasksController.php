c78a9f8e0d8207520a4303ca1b36f16033eae006
kenichi-furuya <ken.f.pooh@gmail.com>
2024-08-17 14:53:55 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -29,7 +29,7 @@ class TasksController extends Controller
     {
         $task = new Task;
 
-        // メッセージ作成ビューを表示
+        // task作成ビューを表示
         return view('tasks.create', [
             'task' => $task,
         ]);
@@ -40,9 +40,16 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        // メッセージを作成
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',   // 追加
+        ]);
+        
+        // taskを作成
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;    // 追加
         $task->save();
 
         // トップページへリダイレクトさせる
@@ -82,10 +89,17 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required|max:255',
+            'status' => 'required|max:10',   // 追加
+        ]);
+        
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
         $task->content = $request->content;
+        $task->status = $request->status;    // 追加
         $task->save();
 
         // トップページへリダイレクトさせる
