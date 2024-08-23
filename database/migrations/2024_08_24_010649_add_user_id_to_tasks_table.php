8ceedd5cdc2fdc35db0e6db4ea406d1793df247c
kenichi-furuya <ken.f.pooh@gmail.com>
2024-08-23 16:22:30 +0000

    lesson20_user_id_adding

--- database/migrations/2024_08_24_010649_add_user_id_to_tasks_table.php
+++ database/migrations/2024_08_24_010649_add_user_id_to_tasks_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('user_id');
+            //
         });
     }
 
@@ -22,8 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->dropColumn('user_id');
-            $table->dropForeign(['user_id']);
+            //
         });
     }
 };
